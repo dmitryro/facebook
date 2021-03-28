d26d8fdb889be09088c5d957c39aa7a42d54ac0f
dmitryro <dmitryro@gmail.com>
2021-03-27 21:53:19 -0400

    roman to integer

--- leetcode/13/roman_to_integer.py
+++ leetcode/13/roman_to_integer.py
@@ -1,20 +1,16 @@
-import re
-
-class RomanNumberHelper:
-    def __init__(self):
-        self.alist = [(1000,'M'), (900,'CM') , (500,'D') , (400 , 'CD'),
-                      (100,'C'),(90,'XC'), (50,'L'), (40, 'XL') ,
-                      (10,'X'), (9,'IX'), (5,'V'),(4,'IV'),(1,'I')]
-
-
-    def from_roman(self, roman):
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        import re
+        alist = [(1000,'M'), (900,'CM') , (500,'D') , (400 , 'CD'),
+                      (100,'C'),(90,'XC'), (50,'L'), (40, 'XL') , (10,'X'),
+                      (9,'IX'), (5,'V'),(4,'IV'),(1,'I')]
         result = 0
         i = 0
-        while i < len(self.alist):
-            reg = '^'+self.alist[i][1]
-            if re.search(reg , roman) != None:
-                result += self.alist[i][0]
-                roman = re.sub(reg,'',roman)
+        while i < len(alist):
+            reg = '^'+alist[i][1]
+            if re.search(reg , s) != None:
+                result += alist[i][0]
+                s = re.sub(reg,'',s)
             else:
                 i+=1
         return result
